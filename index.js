5ec3ee101d63dc97fb3131c7831202629540c128
erias04 <github.eliascerne@icloud.com>
2022-05-04 18:21:46 +0200

    feat: initialized wrangler files

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  event.respondWith(handleRequest(event))
 })
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
-
-
-async function handleRequest(request) {
-  if (request.method === 'GET') {
-    return new Response('Hello Worker', {
-      headers: {
-        'content-type': 'text/plain',
-        status: 200
-      }
-    });
-  } else {
-    return new Response(`Method ${request.method} not allowed`, {
-      headers: {
-        'content-type': 'text/plain',
-        status: 405
-      }
-    })
-  }
+async function handleRequest(event) {
+  return new Response('Hello worker!', {
+    headers: { 'content-type': 'text/plain' },
+  })
 }
