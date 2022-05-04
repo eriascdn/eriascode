4e93873f147dd902777fecd6def833083e0a80a6
erias04 <github.eliascerne@icloud.com>
2022-05-04 14:45:28 +0200

    feat: ask for method; only allow GET methods

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ addEventListener('fetch', event => {
  * Respond with hello worker text
  * @param {Request} request
  */
+
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  if (request.method === 'GET') {
+    return new Response('Hello Worker', {
+      headers: {
+        'content-type': 'text/plain',
+        status: 200
+      }
+    });
+  } else {
+    return new Response(`Method ${request.method} not allowed`, {
+      headers: {
+        'content-type': 'text/plain',
+        status: 405
+      }
+    })
+  }
 }
