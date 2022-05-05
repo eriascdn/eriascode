05b54ef92fbe1ad74a0aa9c93ddcd880076695e2
erias04 <github.eliascerne@icloud.com>
2022-05-05 22:56:44 +0200

    feat: added ./ to path

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    target: 'webworker',
-    context: __dirname,
-    entry: 'src/index.js',
-    mode: 'development',
-    devtool: 'cheap-module-source-map'
+  target: 'webworker',
+  context: __dirname,
+  entry: './src/index.js',
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
 }
