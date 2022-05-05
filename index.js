1992d83abd9a2bf3cbecdc12f4545dcee320cdb7
erias04 <github.eliascerne@icloud.com>
2022-05-05 22:36:57 +0200

    feat: inititialized worker again

--- index.js
+++ index.js
@@ -5,22 +5,8 @@ addEventListener('fetch', event => {
  * Respond with hello worker text
  * @param {Request} request
  */
-
-
 async function handleRequest(request) {
-  if (request.method === 'GET') {
-    return new Response('Hello Worker', {
-      headers: {
-        'content-type': 'text/plain',
-        status: 200
-      }
-    });
-  } else {
-    return new Response(`Method ${request.method} not allowed`, {
-      headers: {
-        'content-type': 'text/plain',
-        status: 405
-      }
-    })
-  }
+  return new Response('Hello worker!', {
+    headers: { 'content-type': 'text/plain' },
+  })
 }
