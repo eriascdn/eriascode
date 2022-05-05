6fce5f9bdc4a2b36907798c215fe1f4b5615750f
erias04 <github.eliascerne@icloud.com>
2022-05-05 18:03:51 +0200

    feat: changed request with event

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event))
+  event.respondWith(handleRequest(event.request))
 })
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
-async function handleRequest(event) {
+async function handleRequest(request) {
   return new Response('Hello worker!', {
     headers: { 'content-type': 'text/plain' },
   })
